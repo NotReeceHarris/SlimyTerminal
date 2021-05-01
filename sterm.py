7005b587c0805e90c7db5b02f19cae4b00b19ebb
NotReeceHarris <reece.harris98@protonmail.com>
2021-05-01 19:43:47 +0100

    modified:   sterm.py 	modified:   utils/ui.py

--- sterm.py
+++ sterm.py
@@ -2,11 +2,8 @@
 
 import os
 import json
-import socket
-import getpass
-import random
 
-from utils.ui import color, __clear__
+from utils.ui import color, __clear__, tabBroker, spacerBroker
 from utils.exitCodes import exitCodes
 from utils.helpMenu import helpMenu
 from utils.configMenu import configMenu
@@ -26,117 +23,68 @@ def logsMenu():
     print('        │\n        └─────────────────────────────────────────────────')
 
 def start():
-
+    
     __statusCode__ = ''
 
     while True:
-
-        __tabs__ = f''
-        __spacer__ = ''
-        __cursor__ = ''
-
+        __tabs__ = tabBroker(__configPath__)
+        __spacer__ = spacerBroker(__configPath__, __statusCode__)[0]
+        __cursor__ = spacerBroker(__configPath__, __statusCode__)[1]
         config = json.load(open(__configPath__))
         
-        try:
 
-            if config['terminalLayout']['dir']: 
-                if os.name == 'nt':
-                    __tabs__ += f'─[ {color("RESET") + os.getcwd()[:3]}{color(config["terminalColor"]) + os.getcwd()[3:] + color("RESET")} ]'.replace('\\',color("RESET")+"\\"+color(config["terminalColor"]))
-                else:
-                    __tabs__ += f'─[ ~{color(config["terminalColor"]) + os.getcwd() + color("RESET")} ]'.replace('/', color("RESET")+'/'+color(config["terminalColor"] ))
-
-
-            if config['terminalLayout']['user']: 
-                __tabs__ += f'─[ {color(config["terminalColor"]) +  getpass.getuser().lower().replace(" ",color("RESET")+"-"+color(config["terminalColor"])) + color("RESET")}@{color(config["terminalColor"]) +  socket.gethostname().replace("-",color("RESET")+"-"+color(config["terminalColor"])) + color("RESET")} ]'
-            
-            if config['terminalLayout']['ip']:
-                
-                LocIp = '-----'
-
-                try:
-                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    s.connect(("8.8.8.8", 80))
-                    LocIp = s.getsockname()[0]
-                    s.close()
-
-                except:
-                    pass
-
-                __tabs__ += f'─[ {color(config["terminalColor"]) + LocIp + color("RESET")} ]'.replace('.',color("RESET")+"."+color(config["terminalColor"]))
-
-            if not config['terminalLayout']['dir'] and not config['terminalLayout']['user'] and not config['terminalLayout']['ip']:
-                __spacer__ = u'\033[F'
-                __cursor__ = '──>'
-
-            elif config['terminalLayout']['dir'] or config['terminalLayout']['user'] or config['terminalLayout']['ip']:
-
-                if config['showExitCode'] and __statusCode__ != '':
-                    __spacer__ = '├──'
-                    __cursor__ = '└─>'
-
-                else:
-                    __spacer__ = '┌──'
-                    __cursor__ = '└─>'
-
-            elif not config['terminalLayout']['dir'] and not config['terminalLayout']['user'] and not config['terminalLayout']['ip'] and config['showExitCode']:
-                __spacer__ = u'\033[F'
-                __cursor__ = '└─>'
-
-            if __statusCode__ == '' or config['showExitCode'] == False:
-                i = str(input(f'''
+        if __statusCode__ == '' or config['showExitCode'] == False:
+            i = str(input(f'''
     {__spacer__}{__tabs__}
     {__cursor__} '''))
 
-            elif __statusCode__ != '' and config['showExitCode'] == True:
-                i = str(input(f'''
+        elif __statusCode__ != '' and config['showExitCode'] == True:
+            i = str(input(f'''
     ┌───[ {color(config["terminalColor"]) + __statusCode__ + color('RESET')} ]
     {__spacer__}{__tabs__}
     └─> '''))
 
-            print(' ')
+        print(' ')
 
-            __logs__.append(str(i))
-            __statusCode__ = ''
+        __logs__.append(str(i))
+        __statusCode__ = ''
 
-            if config['clearEachCommand']: __clear__()
+        if config['clearEachCommand']: __clear__()
 
-            if i.lower() == ':quit' or i.lower() == ':q': 
-                break
+        if i.lower() == ':quit' or i.lower() == ':q': 
+            break
 
-            elif i.lower() == ':help' or i.lower() == ':h': 
-                helpMenu(__configPath__)
+        elif i.lower() == ':help' or i.lower() == ':h': 
+            helpMenu(__configPath__)
 
-            elif i.lower() == ':logs' or i.lower() == ':l': 
-                logsMenu()
+        elif i.lower() == ':logs' or i.lower() == ':l': 
+            logsMenu()
 
-            elif i.lower() == ':clear' or i == ':c': 
-                __clear__()
+        elif i.lower() == ':clear' or i == ':c': 
+            __clear__()
 
-            elif i.lower() == ':config' or i == ':C': 
-                configMenu(__configPath__)
+        elif i.lower() == ':config' or i == ':C': 
+            configMenu(__configPath__)
 
-            elif i.lower() == ':github' or i.lower() == ':g': 
-                print('https://github.com/NotReeceHarris/SlimyTerminal')
+        elif i.lower() == ':github' or i.lower() == ':g': 
+            print('https://github.com/NotReeceHarris/SlimyTerminal')
 
-            elif i.lower() == ':version' or i.lower() == ':v': 
-                checkUpdate(__Version__)
-                print(f'Slimy Terminal: {__Version__}')
+        elif i.lower() == ':version' or i.lower() == ':v': 
+            checkUpdate(__Version__)
+            print(f'Slimy Terminal: {__Version__}')
             
-            elif i.startswith('cd'):
-                try:
-                    os.chdir(i[3:])
-                    __statusCode__ = 'Success'
-                except:
-                    print(f'Unknown Directory "{color(config["terminalColor"]) + i[3:] + color("RESET")}"')
+        elif i.startswith('cd'):
+            try:
+                os.chdir(i[3:])
+                __statusCode__ = 'Success'
+            except:
+                print(f'Unknown Directory "{color(config["terminalColor"]) + i[3:] + color("RESET")}"')
 
-                    __statusCode__ = 'Path not found (1)'
+                __statusCode__ = 'Path not found (1)'
 
 
-            else:
-                __statusCode__ = exitCodes(os.system(i), os.name)
-
-        except ValueError:
-            pass
+        else:
+            __statusCode__ = exitCodes(os.system(i), os.name)
 
 if __name__ == '__main__':
     if os.name == 'nt':
--- utils/ui.py
+++ utils/ui.py
@@ -1,4 +1,7 @@
 import os
+import json
+import getpass
+import socket
 
 def __clear__():        # Terminal wipe module
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -13,3 +16,68 @@ def color(C):           # Color selection module
     elif C == 'CYAN':       return u'\u001b[36m'
     elif C == 'RESET':      return u'\u001b[0m'
     else:                   return u'\u001b[0m'
+
+def tabBroker(__configPath__):
+    __tabs__ = ''
+    config = json.load(open(__configPath__))
+
+    if config['terminalLayout']['dir']: 
+        activeDir = '-----'
+        try:
+            activeDir = os.getcwd()
+            if os.name == 'nt':
+                __tabs__ += f'─[ {color("RESET") + activeDir[:3]}{color(config["terminalColor"]) + activeDir[3:] + color("RESET")} ]'.replace('\\',color("RESET") + "\\" + color(config["terminalColor"]))
+            else:
+                __tabs__ += f'─[ ~{color(config["terminalColor"]) + activeDir + color("RESET")} ]'.replace('/', color("RESET") + '/' + color(config["terminalColor"] ))
+        except:
+            __tabs__ += f'─[ {color(config["terminalColor"]) + activeDir + color("RESET")} ]'
+
+
+    if config['terminalLayout']['user']: 
+        user = '-----'
+        try:
+            user = getpass.getuser()
+            __tabs__ += f'─[ {color(config["terminalColor"]) +  user.lower().replace(" ",color("RESET") + "-" + color(config["terminalColor"])) + color("RESET")}@{color(config["terminalColor"]) +  socket.gethostname().replace("-",color("RESET") + "-" + color(config["terminalColor"])) + color("RESET")} ]'
+        except:
+            __tabs__ += f'─[ {color(config["terminalColor"]) + user + color("RESET")} ]'
+
+
+    if config['terminalLayout']['ip']:
+        locIp = '-----'
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            locIp = s.getsockname()[0]
+            s.close()
+
+            __tabs__ += f'─[ {color(config["terminalColor"]) + locIp + color("RESET")} ]'.replace('.',color("RESET") + "." + color(config["terminalColor"]))
+        except:
+            __tabs__ += f'─[ {color(config["terminalColor"]) + locIp + color("RESET")} ]'
+    
+    return __tabs__
+
+def spacerBroker(__configPath__, __statusCode__):
+
+    __spacer__ = ''
+    __cursor__ = ''
+    config = json.load(open(__configPath__))
+
+    if not config['terminalLayout']['dir'] and not config['terminalLayout']['user'] and not config['terminalLayout']['ip']:
+        __spacer__ = u'\033[F'
+        __cursor__ = '──>'
+
+    elif config['terminalLayout']['dir'] or config['terminalLayout']['user'] or config['terminalLayout']['ip']:
+
+        if config['showExitCode'] and __statusCode__ != '':
+            __spacer__ = '├──'
+            __cursor__ = '└─>'
+
+        else:
+            __spacer__ = '┌──'
+            __cursor__ = '└─>'
+
+    elif not config['terminalLayout']['dir'] and not config['terminalLayout']['user'] and not config['terminalLayout']['ip'] and config['showExitCode']:
+        __spacer__ = u'\033[F'
+        __cursor__ = '└─>'
+
+    return (__spacer__, __cursor__)
